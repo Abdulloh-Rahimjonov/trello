ae5285e0b9a82019ad9816f91371ad67ed6f58a1
Bismillah <rahimjonov333@gmail.com>
2025-04-28 15:58:22 +0300

    added postgres commit

--- src/main/java/uz/pdp/trello/entity/User.java
+++ src/main/java/uz/pdp/trello/entity/User.java
@@ -1,11 +1,7 @@
 package uz.pdp.trello.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import jakarta.persistence.*;
+import lombok.*;
 
 @Data
 @AllArgsConstructor
