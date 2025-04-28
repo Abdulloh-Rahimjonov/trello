e2835c5a39354a471891a0e57ca065be89832f7c
Bismillah <rahimjonov333@gmail.com>
2025-04-28 13:30:45 +0300

    added entity commit

---
+++ src/main/java/uz/pdp/trello/entity/User.java
@@ -0,0 +1,19 @@
+package uz.pdp.trello.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Builder
+public class User {
+
+    @Id
+    private int id;
+}
