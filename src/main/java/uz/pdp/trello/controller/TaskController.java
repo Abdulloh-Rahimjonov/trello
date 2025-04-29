f88ed69e047456b6715aa9788a9a0e61e6c01f72
Abduqayum <abdukayumsss777@gmail.com>
2025-04-30 00:09:24 +0500

    add task

--- src/main/java/uz/pdp/trello/controller/TaskController.java
+++ src/main/java/uz/pdp/trello/controller/TaskController.java
@@ -1,20 +1,27 @@
 package uz.pdp.trello.controller;
 
+import jakarta.mail.Multipart;
+import jakarta.servlet.annotation.MultipartConfig;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+import uz.pdp.trello.entity.Attachment;
 import uz.pdp.trello.entity.Status;
 import uz.pdp.trello.entity.Task;
 import uz.pdp.trello.entity.User;
+import uz.pdp.trello.repo.AttachmentRepository;
 import uz.pdp.trello.repo.StatusRepository;
 import uz.pdp.trello.repo.TaskRepository;
 import uz.pdp.trello.repo.UserRepository;
 
+import java.io.IOException;
 import java.util.List;
 import java.util.Optional;
-
+@MultipartConfig
 @Controller()
 @RequestMapping("/task")
 public class TaskController {
@@ -22,11 +29,13 @@ public class TaskController {
     private final TaskRepository taskRepository;
     private final StatusRepository statusRepository;
     private final UserRepository userRepository;
+    private final AttachmentRepository attachmentRepository;
 
-    public TaskController(TaskRepository taskRepository, StatusRepository statusRepository, UserRepository userRepository) {
+    public TaskController(TaskRepository taskRepository, StatusRepository statusRepository, UserRepository userRepository, AttachmentRepository attachmentRepository) {
         this.taskRepository = taskRepository;
         this.statusRepository = statusRepository;
         this.userRepository = userRepository;
+        this.attachmentRepository = attachmentRepository;
     }
 
     @GetMapping
@@ -88,4 +97,41 @@ public class TaskController {
         }
         return "redirect:/task";
     }
+
+    @GetMapping("/addTaskPage")
+    public String addTaskPage(Model model , @RequestParam Integer userId) {
+        model.addAttribute("userId", userId);
+        return "taskAddPage";
+    }
+    @Transactional
+    @PostMapping("/addTask")
+    public String addTask(@RequestParam Integer userId,
+                          @RequestParam String title,
+                          @RequestParam MultipartFile photo,
+                          Model model) throws IOException {
+
+        Optional<User> user = userRepository.findById(userId);
+        Optional<Status> status = statusRepository.findByName("OPEN");
+
+        if (user.isEmpty() || status.isEmpty()) {
+            model.addAttribute("error", "User or Status not found");
+            return "taskHomePage";
+        }
+
+        Attachment attachment = new Attachment();
+        attachment.setContent(photo.getBytes());
+        attachment.setFile_type(".jpg");
+        attachmentRepository.save(attachment);
+
+        Task task = new Task();
+        task.setUser(user.get());
+        task.setStatus(status.get());
+        task.setTitle(title);
+        task.setAttachment(attachment);
+
+        taskRepository.save(task);
+
+        return "redirect:/task";
+    }
+
 }
--- src/main/java/uz/pdp/trello/repo/StatusRepository.java
+++ src/main/java/uz/pdp/trello/repo/StatusRepository.java
@@ -1,15 +1,16 @@
 package uz.pdp.trello.repo;
 
-import org.springframework.data.domain.Example;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import uz.pdp.trello.entity.Status;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface StatusRepository extends JpaRepository<Status, Integer> {
 
     @Query("SELECT s FROM Status s WHERE s.isActive = true ORDER BY s.positionNumber")
     List<Status> findActiveOrdered();
 
+    Optional<Status> findByName(String name);
 }
